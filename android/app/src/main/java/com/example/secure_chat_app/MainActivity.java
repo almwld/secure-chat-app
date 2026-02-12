eb61f0e3339113558159ca4b261998b758d75cc2
almwld <your-email@example.com>
2026-02-12 06:02:55 +0300

    Legendary Fix: Rewrote Gradle configuration to bridge all plugins

--- android/app/src/main/java/com/example/secure_chat_app/MainActivity.java
+++ android/app/src/main/java/com/example/secure_chat_app/MainActivity.java
@@ -1,7 +1,3 @@
 package com.example.secure_chat_app;
-
 import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // Flutter سيقوم بربط الإضافات تلقائياً هنا
-}
+public class MainActivity extends FlutterActivity { }
